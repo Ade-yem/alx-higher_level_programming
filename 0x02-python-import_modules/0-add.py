6fc5cd4f12da0fc0bdd2643a84cc8e872fea3db2
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2022-12-01 11:02:36 -0800

    hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-addition = add_0.add(a, b)
-print(f"{a} + {b} = {addition}")
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    addition = add(a, b)
+    print(f"{a} + {b} = {addition}")
