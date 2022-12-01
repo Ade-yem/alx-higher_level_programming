031d100f6027aa22ff63de7ab889ce22b157b8c8
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2022-12-01 12:46:04 -0800

    up to 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    n = len(argv)
+    if n == 1:
+        print("0 arguments.")
+    elif n == 2:
+        print("1 argument:")
+        print(f"1: {argv[1]}")
+    else:
+        print(f"{n-1} argument:")
+        for i in range(1, n):
+            print(f"{i}: {argv[i]}")
