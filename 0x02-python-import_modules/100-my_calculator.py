6637c7488d0013b87e4bd65a0c204ecdcefba63c
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2022-12-01 15:13:14 -0800

    imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     elif op == "*":
         print(f"{a} * {b} = {mul(a, b)}")
     else:
-        print(f"{a} / {b} = {divb(a, b)}")
+        print(f"{a} / {b} = {div(a, b)}")
