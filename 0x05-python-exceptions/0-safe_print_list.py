d47adf1bd9050e5a6752edb5d31bc0bade64d5e4
Adejumo Adeyemi <93265896+Ade-yem@users.noreply.github.com>
2022-12-19 22:23:04 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	k = 0
-	for i in range(0, x):
-		try:
-			print(my_list[i], end="")
-		except:
-			break
-		else:
-			k += 1
-
-	print()
-	return k
+    k = 0
+    for i in range(0, x):
+	try:
+	    print(my_list[i], end="")
+	except:
+	    break
+	else:
+	    k += 1
+    print()
+    return k
