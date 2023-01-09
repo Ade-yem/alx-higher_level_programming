513714ede14acc72ccafa640d724c9d287bb2eb6
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-01-09 20:30:39 +0000

    11 print str

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -13,3 +13,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """ Special method that returns a printable string """
+        return "[Square] {}/{}".format(self.__size, self.__size)
