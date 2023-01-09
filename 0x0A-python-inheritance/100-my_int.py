520ea3b00f021b059c54d8d5228ef14adeaef2bb
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-01-09 20:35:16 +0000

    11 print str

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """class MyInt that inherits from int"""
+    i = self.copy
+    return True if i != 3 else False
