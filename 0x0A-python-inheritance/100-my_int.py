b2a97ac076bd0ffe3556c5ac5fc9a2001b1a1a4e
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-01-09 21:06:39 +0000

    last commit

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,10 +7,10 @@ class MyInt(int):
     """class MyInt that inherits from int"""
     def __eq__(self, value):
         """Override == opeartor with != behavior."""
-        #return int.__ne__(self, value)
+        # return int.__ne__(self, value)
         return self.real != value
 
     def __ne__(self, value):
         """Override != operator with == behavior."""
-        #return int.__eq__(self, value)
+        # return int.__eq__(self, value)
         return self.real == value
