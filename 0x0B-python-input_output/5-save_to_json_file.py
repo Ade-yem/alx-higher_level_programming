b2cebf14dc5f2726daba48396631bbfb93d4d6d0
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-01-11 02:55:01 +0000

    pycodestyle

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""function that writes an Object to a text file, using a JSON representation"""
+"""writes an Object to a text file, using a JSON representation"""
 import json
 
 
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,3 @@ def load_from_json_file(filename):
     """a function that creates an Object from a “JSON file”"""
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
-
-
