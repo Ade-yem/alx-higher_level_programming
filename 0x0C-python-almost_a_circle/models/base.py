c66279202b945bf952a266cff1a53e4bd7dba799
Adejumo Adeyemi <93265896+Ade-yem@users.noreply.github.com>
2023-01-16 20:00:16 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,8 +22,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if type(list_dictionaries) == list:
-            if list_dictionaries is None or len(list_dictionaries) == 0:
+            if list_dictionaries is None or list_dictionaries == []:
                 return "[]"
             else:
                 return json.dumps(list_dictionaries)
