add95bd632be227b51bbaf7c50f682c0c82bb6ee
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-01-16 19:11:15 +0000

    initial files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,11 +22,10 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if type(list_dictionaries) == list:
-            if list_dictionaries is None or len(list_dictionaries) == 0:
-                return "[]"
-            else:
-                return json.dumps(list_dictionaries)
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
