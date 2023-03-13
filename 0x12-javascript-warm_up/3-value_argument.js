9b7a14654eaba28657cabc3684d846fae2b2af93
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-03-14 00:07:41 +0100

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,7 @@
+#!/usr/bin/node
 const argv = process.argv;
-let a = 0;
-while (argv[a] !== undefined) {
-  a++;
-}
-if (!argv[2]) {
+if (argv[2]) {
   console.log('No argument');
 } else {
-  for (let i = 2; i < a; i++) {
-    console.log(argv[i]);
-  }
+    console.log(argv[2]);
 }
