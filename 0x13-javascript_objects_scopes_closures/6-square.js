43e251294a7b93604798944e4b7ff66c1d359eca
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-03-14 22:21:13 +0000

    mandatory

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+t Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      let s;
+      for (let i = 0; i < this.height; i++) {
+        s = '';
+        for (let i = 0; i < this.width; i++) {
+          s += c;
+        }
+        console.log(s);
+      }
+    }
+  }
+};
+
