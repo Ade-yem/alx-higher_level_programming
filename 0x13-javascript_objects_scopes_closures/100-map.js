ed03af7d583038d742ef7cb9fc6ddb118032c70b
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-03-14 23:33:50 +0000

    advanced

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const lists = require('./100-data').list;
-const lis = lists.map(x => x * lists.indexOf(x));
+const lis = lists.map((item, index) => item * index);
 console.log(lists);
 console.log(lis);
