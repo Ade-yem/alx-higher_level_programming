340c68ca38c8c62506dc8729092e92fc03eb6f55
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-03-14 22:34:22 +0000

    standard check

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,23 +1,19 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareP = require('./5-square');
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends SquareP {
   charPrint (c) {
     if (c === undefined) {
-      this.print();
-    } else {
-      let s;
-      for (let i = 0; i < this.height; i++) {
-        s = '';
-        for (let i = 0; i < this.width; i++) {
-          s += c;
-        }
-        console.log(s);
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
       }
+      console.log(s);
     }
   }
-};
+}
+
+module.exports = Square;
