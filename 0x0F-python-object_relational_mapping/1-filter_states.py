8697366a1a52269fa3fdb0a32092a63dba6384ed
Adejumo Adeyemi <93265896+Ade-yem@users.noreply.github.com>
2023-03-18 23:32:50 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" script that lists all states from the database hbtn_0e_0_usa"""
+"""
+Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+Usage: ./1-filter_states.py <mysql username> <mysql password>
+                             <database name>
+"""
 import sys
 import MySQLdb
 
@@ -8,5 +12,3 @@ if __name__ == "__main__":
     c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY `id`")
     [print(state) for state in c.fetchall() if state[1][0] == "N"]
-    cur.close()
-    conn.close()
