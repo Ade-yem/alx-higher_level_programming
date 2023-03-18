44ea4d72a18bcc9a68d47a1d0a16718fa0f01125
Adejumo Adeyemi <93265896+Ade-yem@users.noreply.github.com>
2023-03-18 23:30:40 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """ script that lists all states from the database hbtn_0e_0_usa"""
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
     cur.close()
     conn.close()
