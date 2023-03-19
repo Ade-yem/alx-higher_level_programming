5e4dbb26ec8600cb8ed1d17ce58aa5f6263ee2c0
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-03-18 17:57:51 -0700

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     n = 0
-    for state in session.query(State).order_by(State.id).all():
+    for state in session.query(State):
         if state.name == sys.argv[4]:
             print("{}".format(state.id))
             n = 1
