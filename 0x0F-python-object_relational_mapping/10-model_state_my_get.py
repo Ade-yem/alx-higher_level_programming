fea93e1aa867fe8e4335cfc0c1e94efe5c9368f7
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-03-18 17:49:53 -0700

    fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     session = Session()
     n = 0
     for state in session.query(State):
-        if state == sys.argv[4]:
+        if state.name == sys.argv[4]:
             print("{}".format(state.id))
             n = 1
             break
