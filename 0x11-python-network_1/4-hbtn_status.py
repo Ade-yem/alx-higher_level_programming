178b3ae2c9a6efef1f05ea3583b498af1ebd87e7
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-03-31 16:21:16 -0700

    solved code fail for this task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
 
     r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {type(r.text)}")
-    print("\t- content: {r.text}")
+    print(f"\t- type: {type(r.text)}")
+    print(f"\t- content: {r.text}")
