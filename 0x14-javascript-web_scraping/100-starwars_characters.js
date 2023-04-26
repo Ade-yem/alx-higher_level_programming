d345b2e9b3b87b45fa4b0f14acce832111bbc1b3
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-04-26 11:06:22 -0700

    fixed the apis

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api//films/';
+request.get(url + id, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, response, body) {
+      if (error) {
+        console.log(error);
+      }
+      const result = JSON.parse(body);
+      console.log(result.name);
+    });
   }
 });
