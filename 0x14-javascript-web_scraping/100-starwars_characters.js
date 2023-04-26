7bdb578470e56576505da33540acb3020dfcbe0e
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-04-26 18:26:08 +0000

    initial files

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,20 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api//films/';
-request.get(url + id, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const i of dd) {
-    req.get(i, function (error, response, body) {
-      if (error) {
-        console.log(error);
-      }
-      const result = JSON.parse(body);
-      console.log(result.name);
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name);
+        }
+      });
     });
   }
 });
